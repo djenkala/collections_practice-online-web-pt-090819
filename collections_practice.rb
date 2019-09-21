3baefe82189f938b9290d9b193f6a935b70d09e0
Daniel Jenkala <djenkala@yahoo.com>
2019-09-21 00:58:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,3 +41,15 @@ def sum_array(array)
     sum+=num
   end
   sum
+  
+  end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
